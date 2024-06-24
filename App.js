a9296e71b619b0ad0fc50b66a45bca880423ff76
Alina Orlova <alinaorlova50@mail.ru>
2024-06-24 14:00:57 +0300

    базовый макет приложения

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Tab = createBottomTabNavigator();
+
+function Main() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>
+                Главная
+            </Text>
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function About() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>
+                Топики
+            </Text>
+        </View>
+    )
+}
+
+function Settings() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>
+                Настройки
+            </Text>
+        </View>
+    )
+}
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator initialRouteName="Main">
+                <Tab.Screen name="Main" component={Main} />
+                <Tab.Screen name="About" component={About} />
+                <Tab.Screen name="Settings" component={Settings} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
---
+++ styles/style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export const gStyle = StyleSheet.create({
+    main: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 20,
+        color: '#333',
+        fontFamily: 'Ubuntu-Bold',
+    },
+});
